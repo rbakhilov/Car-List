f4b03b8341637b7ae778081f0ab612990ed80c00
rbakhilov <roman.bakhilov@gmail.com>
2017-07-14 14:18:14 +0300

    Revert "Revert "Doesn't work add.""
    This reverts commit c6c95c20ad316ff2367042dde124f0eef323ed76.

---
+++ HomeWork3/CarCell.swift
@@ -0,0 +1,28 @@
+//
+//  CarCell.swift
+//  HomeWork3
+//
+//  Created by Роман on 14.07.17.
+//  Copyright © 2017 GoIT. All rights reserved.
+//
+
+import UIKit
+
+class CarCell: UITableViewCell {
+    
+    @IBOutlet weak var makeLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
